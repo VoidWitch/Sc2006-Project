628db43274e064ee63dfceb26d5fea7ae10b2652
POI <108055046+VoidWitch@users.noreply.github.com>
2024-03-20 11:43:13 +0800

    Update RegisterUser.tsx

--- RegisterUser.tsx
+++ RegisterUser.tsx
@@ -1,5 +1,6 @@
 import { TextInput } from "react-native-paper"
 
+//implement register new user login screen
 const RegisterNewUser = () => {
     return (
         <TextInput>Register new user</TextInput>
